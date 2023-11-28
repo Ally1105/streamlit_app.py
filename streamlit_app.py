56a0ab8ae98a0924b19d076d9d79d3cd80b670e7
Ally1105 <150768965+Ally1105@users.noreply.github.com>
2023-11-28 09:47:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,45 +31,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-# New SEction to display fruityvuce API response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
- 
-except URLError as e:
-  streamlit.error()
-#streamlit.stop() 
-streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
-def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur:
-            my_cur.execute("select * from fruit_load_list")
-            return my_cur.fetchall()
-#add a button to load the fruit
-if streamlit.button('Get Fruit List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      my_cnx.close()
-      streamlit.dataframe(my_data_rows) 
-#Allow the end user to add fruit
-def insert_row_snowflake(new_fruit):
-      with my_cnx.cursor() as my_cur:
-            my_cur.execute("insert into fruit_load_list values ('" + 'jackfruit' "')")
-            #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-            return "Thanks for adding " + new_fruit
- 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')     
-if streamlit.button('Add a Fruit to the List'):     
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      back_from_function = insert_row_snowflake (add_my_fruit)
-      streamlit.text(back_from_function)
- 
-      
-#streamlit.write(')
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
